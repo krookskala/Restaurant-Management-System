6a887c416c9ce5ea523f2b3218780b106cdcddb4
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2025-01-21 11:06:05 +0100

    Added Validation

---
+++ ConsoleApp1/Validation/NonDefaultDateAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp1.Validation
+{
+    public class NonDefaultDateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is DateTime dateValue && dateValue == DateTime.MinValue)
+            {
+                return new ValidationResult(ErrorMessage ?? "Date cannot be the default value (01/01/0001).");
+            }
+            return ValidationResult.Success!;
+        }
+    }
+}
